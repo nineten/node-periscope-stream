479b069c66d87570b21e12e4c158b0f8afa4bb08
nineten <jayden87@gmail.com>
2016-01-07 11:43:35 +0800

    added a check to ensure no array out of bound error for url checking

--- periscope.js
+++ periscope.js
@@ -1,20 +1,26 @@
 var request = require('request');
 
 function periscope(url, callback) {
-	var id = url.match(/periscope.tv\/w\/(.*)/i)[1];
-	
-	var api = 'https://api.periscope.tv/api/v2/getAccessPublic?broadcast_id=' + id;
-	
-	request(api, function(error, response, body) {
-		if (error || response.statusCode != 200) {
-			callback('Response error: ' + response.statusCode);
-			return;
-		}
+	var urlmatch = url.match(/periscope.tv\/w\/(.*)/i)
+
+	if (urlmatch === null) {
+		callback('Url error: Not a periscope url');
 		
-		var obj = JSON.parse(body);
+	} else {
+		var id = urlmatch[1];
+		var api = 'https://api.periscope.tv/api/v2/getAccessPublic?broadcast_id=' + id;
 		
-		callback(null, obj);
-	});
+		request(api, function(error, response, body) {
+			if (error || response.statusCode != 200) {
+				callback('Response error: ' + response.statusCode);
+				return;
+			}
+			
+			var obj = JSON.parse(body);
+			
+			callback(null, obj);
+		});
+	}
 }
 
 module.exports = periscope;
